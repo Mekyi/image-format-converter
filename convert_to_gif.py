5e2671d3cdce44ceb3d444958e5b05b0864ec770
Mikael Björke <contact@mbjorke.com>
2024-03-14 11:37:41 +0200

    Check files to be converted from input directory instead of from root

--- convert_to_gif.py
+++ convert_to_gif.py
@@ -6,25 +6,27 @@ import pillow_avif
 current_path = os.path.dirname(os.path.abspath(__file__))
 formats_to_convert = ('avif', 'webp')
 files_to_convert = []
+input_path = os.path.join(current_path, 'input')
 output_path = os.path.join(current_path, 'output')
 
 if not os.path.exists(output_path):
     os.makedirs(output_path)
 
 # Get list of files to convert from current path
-dir_list = os.listdir(current_path)
+dir_list = os.listdir(input_path)
 
 for file_name in dir_list:
     if file_name.endswith(formats_to_convert):
-        files_to_convert.append(file_name)
+        files_to_convert.append(os.path.join(input_path, file_name))
 
 print(f"Files to convert: {files_to_convert}")
 
 # Convert files in a loop to gif
-for file in files_to_convert:
-    file_name, extension = os.path.splitext(file)
+for file_path in files_to_convert:
+    file_path_no_ext, extension = os.path.splitext(file_path)
+    file_name = os.path.split(file_path_no_ext)[1]
     path_to_save = os.path.join(output_path, f'{file_name}.gif')
-    im = Image.open(file)
+    im = Image.open(file_path)
     im.info.pop('background', None)
     # Fix quality
     im.save(path_to_save, 
